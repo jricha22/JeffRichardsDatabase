41e1fdefb2f1b38c75b9ee3e05a6f3c08a405924
Jeff Richards <jeff.richards@earthlink.net>
2016-02-13 14:52:18 -0500

    Add button handlers that implement the 3 activities to the database

--- app/src/main/java/edu/westga/jeffrichardsdatabase/DatabaseActivity.java
+++ app/src/main/java/edu/westga/jeffrichardsdatabase/DatabaseActivity.java
@@ -2,12 +2,70 @@ package edu.westga.jeffrichardsdatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DatabaseActivity extends AppCompatActivity {
+
+    private TextView idView;
+    private EditText productBox;
+    private EditText quantityBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+
+        this.idView = (TextView) findViewById(R.id.productID);
+        this.productBox = (EditText) findViewById(R.id.productName);
+        this.quantityBox =
+                (EditText) findViewById(R.id.productQuantity);
+    }
+
+    public void newProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        int quantity =
+                Integer.parseInt(quantityBox.getText().toString());
+
+        Product product =
+                new Product(productBox.getText().toString(), quantity);
+
+        dbHandler.addProduct(product);
+        productBox.setText("");
+        quantityBox.setText("");
+    }
+
+    public void lookupProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        Product product =
+                dbHandler.findProduct(productBox.getText().toString());
+
+        if (product != null) {
+            idView.setText(String.valueOf(product.getID()));
+
+            quantityBox.setText(String.valueOf(product.getQuantity()));
+        } else {
+            idView.setText(getResources().getString(R.string.no_match_found));
+        }
+    }
+
+    public void removeProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null,
+                null, 1);
+
+        boolean result = dbHandler.deleteProduct(
+                productBox.getText().toString());
+
+        if (result)
+        {
+            idView.setText(getResources().getString(R.string.record_deleted));
+            productBox.setText("");
+            quantityBox.setText("");
+        }
+        else
+            idView.setText(getResources().getString(R.string.no_match_found));
     }
 }
